016cebd615e486b426806e73e3196dabcf29b2f3
angeldagoldol <angel_dagoldol@sjp2cd.edu.ph>
2026-08-18 03:39:45 +0800

    Update config.js

--- config.js
+++ config.js
@@ -15,8 +15,8 @@ window.SUPABASE_ANON_KEY =
 window.DAGOLDOL_CONFIG = Object.freeze({
   PHASE2_ENABLED: true,
   PHASE3_ENABLED: true,
-  VERSION: "3.2.2",
-  ASSET_VERSION: "3.2.2",
+  VERSION: "3.3.0",
+  ASSET_VERSION: "3.3.0",
   SITE_URL: "https://lorenz-web-six.vercel.app"
 });
 
